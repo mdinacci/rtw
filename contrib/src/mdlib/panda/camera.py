a7d5d2bb29d101dc4b50d7a05d7f978ac82b1e57
Marco Dinacci <mdinacci@viuulenza.(none)>
2008-12-18 13:37:18 +0100

    fully implemented AbstractCamera and FreeCamera

--- contrib/src/mdlib/panda/camera.py
+++ contrib/src/mdlib/panda/camera.py
@@ -6,35 +6,13 @@ Author: Marco Dinacci <dev@dinointeractive.com>
 This module contains a collection of different cameras
 """
 
+__all__  = ["FreeCamera","FixedCamera"]
+
 import direct.directbase.DirectStart
-from pandac.PandaModules import Vec3, NodePath
+from direct.showbase.DirectObject import DirectObject
+from pandac.PandaModules import Vec3, NodePath, WindowProperties, Camera
 
-
-class CameraManager(object):
-    
-    def __init__(self, inputSystem):
-        self.ins = inputSystem
-        self.cams = {"free": FreeCamera(self.ins), "fixed": None}
-        self.active = self.cams["free"]
-    
-    def setActive(self, cameraName):
-        if cameraName in self.cams.keys():
-            self.active = self.cams[cameraName]
-            self.active.reset()
-
-class AbstractCamera(object):
-    def reset(self):
-        """ Abstract method to override """
-        raise NotImplementedError()
-
-class FixedCamera(AbstractCamera):
-    def __init__(self, pos):
-        base.disableMouse()
-    
-    def reset(self):
-        pass
-        
-class FreeCamera(AbstractCamera):
+class AbstractCamera(Camera):
     FORWARD = Vec3(0,2,0)
     BACK = Vec3(0,-1,0)
     LEFT = Vec3(-1,0,0)
@@ -42,52 +20,116 @@ class FreeCamera(AbstractCamera):
     UP = Vec3(0,0,1)
     DOWN = Vec3(0,0,-1)
     STOP = Vec3(0)
-    walk = STOP
-    strafe = STOP
     
-    def __init__(self, ins):
-        self.createVirtualNode()
-        self.attachControls(ins)
-        self.setupCamera()
+    def __init__(self, name):
+        super(AbstractCamera, self).__init__(name)
         
-        self.linearSpeed = 70
-        taskMgr.add(self.mouseUpdate, 'mouse-task')
-        taskMgr.add(self.moveUpdate, 'move-task')
-
-    def reset(self):
-        pass
-
-    # FIXME InputSystem must be responsible for this !
-    def attachControls(self, ins):
-        ins.bind( "s" , self.__setattr__,["walk",self.STOP] )
-        ins.bind( "w" , self.__setattr__,["walk",self.FORWARD])
-        ins.bind( "s" , self.__setattr__,["walk",self.BACK] )
-        ins.bind( "s-up" , self.__setattr__,["walk",self.STOP] )
-        ins.bind( "w-up" , self.__setattr__,["walk",self.STOP] )
-        ins.bind( "a" , self.__setattr__,["strafe",self.LEFT])
-        ins.bind( "d" , self.__setattr__,["strafe",self.RIGHT] )
-        ins.bind( "a-up" , self.__setattr__,["strafe",self.STOP] )
-        ins.bind( "d-up" , self.__setattr__,["strafe",self.STOP] )
-        ins.bind( "q" , self.__setattr__,["strafe",self.UP] )
-        ins.bind( "q-up" , self.__setattr__,["strafe",self.STOP] )
-        ins.bind( "e" , self.__setattr__,["strafe",self.DOWN] )
-        ins.bind( "e-up" , self.__setattr__,["strafe",self.STOP] )
+        self.walk = self.STOP
+        self.strafe = self.STOP
+        self.linearSpeed = 50
         
-    def createVirtualNode(self):
-        self.node = NodePath("vcam")
-        self.node.reparentTo(render)
-        self.node.setPos(0,0,2)
-        self.node.setScale(0.05)
-        self.node.showBounds()
+        # accept input and configure controls
+        self.ins = DirectObject()
+        self._attachControls()
+        
+        # attach to the base.camera node
+        base.camera.attachNewNode(self)
+        
+        # camera specific initialisation
+        self.initialise()
+        
+        # animate the camera
+        taskMgr.add(self.moveTask, 'move-task')
+    
+    def _attachControls(self):
+        self.ins.accept( "s" , self.__setattr__,["walk",self.STOP] )
+        self.ins.accept( "w" , self.__setattr__,["walk",self.FORWARD])
+        self.ins.accept( "s" , self.__setattr__,["walk",self.BACK] )
+        self.ins.accept( "s-up" , self.__setattr__,["walk",self.STOP] )
+        self.ins.accept( "w-up" , self.__setattr__,["walk",self.STOP] )
+        self.ins.accept( "a" , self.__setattr__,["strafe",self.LEFT])
+        self.ins.accept( "d" , self.__setattr__,["strafe",self.RIGHT] )
+        self.ins.accept( "a-up" , self.__setattr__,["strafe",self.STOP] )
+        self.ins.accept( "d-up" , self.__setattr__,["strafe",self.STOP] )
+        self.ins.accept( "q" , self.__setattr__,["strafe",self.UP] )
+        self.ins.accept( "q-up" , self.__setattr__,["strafe",self.STOP] )
+        self.ins.accept( "e" , self.__setattr__,["strafe",self.DOWN] )
+        self.ins.accept( "e-up" , self.__setattr__,["strafe",self.STOP] )
+        
+    def initialise(self):
+        """ Initialise camera properties like position and hpr """
+        raise NotImplementedError()
+        
+    def getPos(self):
+        return base.camera.getPos()
 
-    def setupCamera(self):
+    def setPos(self, x, y, z):
+        base.camera.setPos(x, y, z)
+        
+    def lookAt(self, x, y, z):
+        base.camera.lookAt(x, y, z)
+
+    def showCursor(self, show = True):
+        """ Hide the mouse cursor """
+        props = WindowProperties()
+        props.setCursorHidden(not show)
+        base.win.requestProperties(props)
+
+    def destroy(self):
+        """ 
+        Very important, it detaches the camera completely from its parent nodepath
+        and stop receving events 
+        
+        TODO check for correctness
+        """
+        self.setActive(False)
+        self.ignoreAll() 
+        base.camera.removeNode(self)
+
+    def moveTask(self,task):
+        """ this task makes the vcam move """
+        # move where the keys set it
+        base.camera.setPos(base.camera,self.walk*globalClock.getDt()*self.linearSpeed)
+        base.camera.setPos(base.camera,self.strafe*globalClock.getDt()*self.linearSpeed)
+        
+        return task.cont
+
+class FixedCamera(AbstractCamera):
+    """
+    This camera is suited to select objects in the scene.
+    It can be moved using the "wasd" key's combination and doesn't 
+    allow any rotation YET.
+    
+    TODO implement rotation
+    """
+    def __init__(self, pos):
+        super(FixedCamera, self).__init__("fixed")
+    
+    def initialise(self):
         base.disableMouse()
-        camera.setPos( 0, -40, 20)
-        camera.lookAt(0,0,0)
-        camera.reparentTo(self.node)
-        pl =  base.cam.node().getLens()
+        self.showCursor(True)
+        self.setActive(True)
+        
+        
+class FreeCamera(AbstractCamera):
+    """
+    FPS-like camera, moving the mouse around will update the position of the camera.
+    """
+    def __init__(self):
+        super(FreeCamera, self).__init__("free")
+        self.linearSpeed = 65
+        self.angularSpeed = 5
+        taskMgr.add(self.mouseUpdate, 'mouse-task')
+
+    def initialise(self):
+        base.disableMouse()
+        #camera.setPos( 0, -40, 20)
+        #camera.lookAt(0,0,0)
+        pl = self.getLens()
         pl.setFov(70)
-        base.cam.node().setLens(pl)
+        self.setLens(pl)
+        self.setActive(True)
+        self.showCursor(False)
     
     def mouseUpdate(self,task):
         """ this task updates the mouse """
@@ -95,14 +137,9 @@ class FreeCamera(AbstractCamera):
         x = md.getX()
         y = md.getY()
         if base.win.movePointer(0, base.win.getXSize()/2, base.win.getYSize()/2):
-            self.node.setH(self.node.getH() -  (x - base.win.getXSize()/2)*0.3)
-            base.camera.setP(base.camera.getP() - (y - base.win.getYSize()/2)*0.3)
+            base.camera.setH(base.camera.getH() -  (x - base.win.getXSize()/2) * 
+                             globalClock.getDt() * self.angularSpeed)
+            base.camera.setP(base.camera.getP() - (y - base.win.getYSize()/2) *
+                             globalClock.getDt() * self.angularSpeed)
+            
         return task.cont
-
-    def moveUpdate(self,task):
-        """ this task makes the vcam move """
-        # move where the keys set it
-        self.node.setPos(self.node,self.walk*globalClock.getDt()*self.linearSpeed)
-        self.node.setPos(self.node,self.strafe*globalClock.getDt()*self.linearSpeed)
-        return task.cont
-    
