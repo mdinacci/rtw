9914e1b6be596fc2495ff6addd528cfe5d175e4b
Marco Dinacci <dev@dinointeractive.com>
2009-01-08 12:10:08 +0100

    added Colors class, fixed decorators

--- contrib/src/mdlib/panda/__init__.py
+++ contrib/src/mdlib/panda/__init__.py
@@ -11,7 +11,21 @@ a Panda3D program
 import os
 
 from direct.showbase.DirectObject import DirectObject
+from pandac.PandaModules import Vec4
 
+class Color(object):
+    # some colors
+    BLACK = Vec4(0,0,0,1)
+    WHITE = Vec4(1,1,1,1)
+    RED = Vec4(1,0,0,1)
+    GREEN = Vec4(0,1,0,1)
+    BLUE = Vec4(0,0,1,1)
+    HIGHLIGHT = Vec4(1,1,0.3,0.5)
+    
+    COLOR_IDX = 0
+    b_n_w = [BLACK,WHITE]
+
+# TODO move to input
 class SafeDirectObject(DirectObject):
     def destroy(self):
         self.ignoreAll() 
@@ -31,28 +45,41 @@ def groupNodes(render, groupName, *nodes):
     
     return groupNode
 
-def editorOnly(fn):
+
+# DECORATORS
+
+def guiCallback(func):
     """ 
-    A decorator which does absolutely nothing but document
-    that a function is used only in the editor  
-    """
+    Decorator that documents that the function is called by the GUI 
+    in response to the user interacting with a widget. """
     def new(*args):
-        # do nothing for the moment
-        return fn(*args)
+        return func(*args)
+    return new
+     
+def inputCallback(func):
+    """ 
+    Decorator that documents that the function is called by the input 
+    system in response to a user action. """
+    def new(*args):
+        return func(*args)
     return new
 
-class ModelNotFoundException(Exception):
-    def __init__(self, modelPath):
-        super(ModelNotFoundException, self).__init__("Model %s not found" % modelPath)
+def eventCallback(func):
+    """ 
+    Decorator that documents that the function is called by the event 
+    system in response to an event just been fired. """
+    def new(*args):
+        return func(*args)
+    return new
 
-def pandaCallback(fn):
+def pandaCallback(func):
     """ 
     A decorator which does absolutely nothing but document
     that a function is a callback called from Panda3D  
     """
     def new(*args):
         # do nothing for the moment
-        return fn(*args)
+        return func(*args)
     return new
 
 class ModelNotFoundException(Exception):
