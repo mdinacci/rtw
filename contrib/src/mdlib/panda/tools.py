020c7b14ca0dfcefab83a9a0f580d6f02bf3c244
Marco Dinacci <dev@dinointeractive.com>
2009-02-12 00:18:16 +0100

    added new function tagify

--- contrib/src/mdlib/panda/tools.py
+++ contrib/src/mdlib/panda/tools.py
@@ -5,16 +5,52 @@ Author: Marco Dinacci <dev@dinointeractive.com>
 Copyright © 2008-2009
 """
 
+from pandac.PandaModules import EggData, EggGroup, Filename
 from subprocess import Popen
 
 
-def tessellate(inputFile, outputFile):
+def tagify(inputFile, group, tagKey, tagValue):
+    """ Add the given tag to the given group. Uses the EGG API """
+    
+    def getGroup(data, group):
+        for child in data.getChildren():
+            if child.getClassType().getName() == "EggGroup" \
+                and child.getName() == group:
+                return child
+            groupNode = getGroup(child.getChildren(), group)
+            if groupNode is not None:
+                return groupNode
+    
+    data = EggData()
+    data.read(Filename.fromOsSpecific(inputFile))
+    
+    group = getGroup(data, group)
+    if group is not None:
+        group.setTag(tagKey, tagValue)
+            
+
+def tessellate(inputFile, outputFile, up, us):
     """ Tessellate an EggNurbsSurface using the egg-qtess tool """
     
-    cmd = "egg-qtess -tbnauto -up 5 -o %s %s" % (outputFile, inputFile)
+    cmd = "egg-qtess -tbnauto -up %d -us %d -o %s %s" % (up, us, 
+                                                         outputFile, inputFile)
     Popen(cmd.split())
+    
 
-def groupify(modelFile, outputFile=None, polysPerGroup=5):
+def holeify(inputFile, outputFile, holesIndexes):
+    """ 
+    Remove polygons from an egg file 
+    TODO: it would be nice to remove vertexes too, but it's not trivial as I 
+          have to check that they're not used by other polygons
+    """
+    f = open(inputFile)
+    f2 = open(outputFile, "w")
+    
+    lines = f.readlines()
+    
+
+
+def groupify(modelFile, outputFile=None, polysPerGroup=5, prefix="row"):
     """ Organise polysPerGroup polygons in a group """
     
     if outputFile == None:
@@ -25,18 +61,18 @@ def groupify(modelFile, outputFile=None, polysPerGroup=5):
 
     inGroup = False
     firstPolygon = True
-    polysCount = 1
+    polysCount = 0
 
     lines = f.readlines()
     for i,line in enumerate(lines):
         if "Polygon" in line:
             if firstPolygon:
                 f2.write("}\n") # close previous group
-                f2.write("<Group> segment-%d {\n" % (polysCount/polysPerGroup))
+                f2.write("<Group> %s-%d {\n" % (prefix, polysCount/polysPerGroup))
                 firstPolygon = False
-            if polysCount % polysPerGroup == 0:
+            elif polysCount % polysPerGroup == 0:
                 f2.write("}\n")
-                f2.write("<Group> segment-%d {\n" % (polysCount/polysPerGroup))
+                f2.write("<Group> %s-%d {\n" % (prefix, polysCount/polysPerGroup))
             polysCount+=1
         f2.write(line)
 
