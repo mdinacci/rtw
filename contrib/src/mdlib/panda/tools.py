9252293860c6d7415062af4244ea0543968497c9
Marco Dinacci <dev@dinointeractive.com>
2009-02-15 01:07:18 +0100

    lots of changes

--- contrib/src/mdlib/panda/tools.py
+++ contrib/src/mdlib/panda/tools.py
@@ -8,7 +8,6 @@ Copyright © 2008-2009
 from pandac.PandaModules import EggData, EggGroup, Filename
 from subprocess import Popen
 
-
 def tagify(inputFile, group, tagKey, tagValue):
     """ Add the given tag to the given group. Uses the EGG API """
     
@@ -32,25 +31,48 @@ def tagify(inputFile, group, tagKey, tagValue):
 def tessellate(inputFile, outputFile, up, us):
     """ Tessellate an EggNurbsSurface using the egg-qtess tool """
     
-    cmd = "egg-qtess -tbnauto -up %d -us %d -o %s %s" % (up, us, 
-                                                         outputFile, inputFile)
+    # first write the parameter file
+    paramFile = "/tmp/curve_params"
+    f = open(paramFile, "w")
+    f.write("curve : %d %d" % (up, us))
+    
+    cmd = "egg-qtess -tbnauto -cs z-up -f %s -o %s %s" % (paramFile,outputFile,inputFile)
+    
     Popen(cmd.split())
     
 
-def holeify(inputFile, outputFile, holesIndexes):
+def holeify(inputFile, outputFile, holesIndexes, prefix="row-"):
     """ 
     Remove polygons from an egg file 
     TODO: it would be nice to remove vertexes too, but it's not trivial as I 
           have to check that they're not used by other polygons
     """
+    
+    polysPerRow = 5
+    polyLines = 4
+
     f = open(inputFile)
     f2 = open(outputFile, "w")
     
+    skipPolygon = False
     lines = f.readlines()
+    polyCount = -1
+    linesToSkip = 0
+    for line in lines:
+        if linesToSkip > 0:
+            linesToSkip-=1
+            continue
+        
+        if "Polygon" in line:
+            polyCount +=1
+            if polyCount in holesIndexes:
+                linesToSkip = 3
+                continue
+        f2.write(line)
     
-
-
-def groupify(modelFile, outputFile=None, polysPerGroup=5, prefix="row"):
+    
+def groupify(modelFile, outputFile=None, polysPerGroup=5, 
+             rowPrefix="row", polyPrefix="tile-"):
     """ Organise polysPerGroup polygons in a group """
     
     if outputFile == None:
@@ -62,18 +84,32 @@ def groupify(modelFile, outputFile=None, polysPerGroup=5, prefix="row"):
     inGroup = False
     firstPolygon = True
     polysCount = 0
-
+    polygonIndex = 0
     lines = f.readlines()
     for i,line in enumerate(lines):
         if "Polygon" in line:
             if firstPolygon:
                 f2.write("}\n") # close previous group
-                f2.write("<Group> %s-%d {\n" % (prefix, polysCount/polysPerGroup))
+                f2.write("<Group> %s-%d {\n" % (rowPrefix, 
+                                                polysCount/polysPerGroup))
+                f2.write("  <Model> { 1 }\n")
+                #f2.write("  <Collide> { Polyset keep descend }\n")
                 firstPolygon = False
             elif polysCount % polysPerGroup == 0:
                 f2.write("}\n")
-                f2.write("<Group> %s-%d {\n" % (prefix, polysCount/polysPerGroup))
+                f2.write("<Group> %s-%d {\n" % (rowPrefix, 
+                                                polysCount/polysPerGroup))
+                #f2.write("  <Collide> { Polyset keep descend }\n")
+                f2.write("  <Model> { 1 }\n")
+            
             polysCount+=1
+            
+            line = "  <Polygon> %s%s {\n" % (polyPrefix, polygonIndex)
+            if polygonIndex < polysPerGroup -1:
+                polygonIndex += 1
+            else:
+                polygonIndex = 0
+            
         f2.write(line)
 
     f.close()
