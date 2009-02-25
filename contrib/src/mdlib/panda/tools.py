d0f2c9059c5435a6ac63e8a5ac08eb3544aa0608
Marco Dinacci <dev@dinointeractive.com>
2009-02-25 17:54:41 +0100

    rewritten most of the tools to use the Egg API

--- contrib/src/mdlib/panda/tools.py
+++ contrib/src/mdlib/panda/tools.py
@@ -7,27 +7,40 @@ Copyright © 2008-2009
 
 from pandac.PandaModules import EggData, EggGroup, Filename
 from subprocess import Popen
+from os import unlink
 
-def tagify(inputFile, group, tagKey, tagValue):
-    """ Add the given tag to the given group. Uses the EGG API """
+
+def modelify(inputFile, outputFile, tagKey, tagValue):
+    """ Add the <Model> entry for each group in the file """
     
-    def getGroup(data, group):
-        for child in data.getChildren():
-            if child.getClassType().getName() == "EggGroup" \
-                and child.getName() == group:
-                return child
-            groupNode = getGroup(child.getChildren(), group)
-            if groupNode is not None:
-                return groupNode
+    f = open(inputFile)
+    f2 = open(outputFile, "w")
+    
+    for line in f:
+        f2.write(line)
+        if "row" in line:
+            f2.write("  <Model> { 1 }\n")
+    f.close()
+    f2.close()
+    
+
+def tagify(inputFile, outputFile, tagKey, tagValue):
+    """ Add the given tag to the given group. Uses the EGG API """
     
     data = EggData()
     data.read(Filename.fromOsSpecific(inputFile))
+    # read out useless stuff
+    child = data.getFirstChild()
+    child = data.getNextChild()
     
-    group = getGroup(data, group)
-    if group is not None:
-        group.setTag(tagKey, tagValue)
-            
+    while child is not None:
+        child = data.getNextChild()
+        if type(child) is EggGroup:
+            child.setTag(tagKey, tagValue)
 
+    data.writeEgg(Filename.fromOsSpecific(outputFile))
+    
+            
 def tessellate(inputFile, outputFile, up, us):
     """ Tessellate an EggNurbsSurface using the egg-qtess tool """
     
@@ -39,46 +52,6 @@ def tessellate(inputFile, outputFile, up, us):
     cmd = "egg-qtess -cs z-up -f %s -o %s %s" % (paramFile,outputFile,inputFile)
     
     Popen(cmd.split())
-    
-
-def holeify(inputFile, outputFile, holesIndexes, prefix="row-", 
-            polysPerRow=5, polyLines=4, linesPerRow=3):
-    """ 
-    Remove polygons from an egg file 
-    TODO: it would be nice to remove vertexes too, but it's not trivial as I 
-          have to check that they're not used by other polygons
-    """
-    print holesIndexes
-    
-    f = open(inputFile)
-    f2 = open(outputFile, "w")
-    
-    indexes = []
-    indexes += map(lambda x: (x/polysPerRow) * polysPerRow * linesPerRow - 
-                   linesPerRow + polysPerRow +1, holesIndexes)
-    indexes += map(lambda x: (x/polysPerRow) * polysPerRow * linesPerRow - 
-                   linesPerRow + polysPerRow*2 +1, holesIndexes)
-    indexes += map(lambda x: (x/polysPerRow) * polysPerRow * linesPerRow - 
-                   linesPerRow + polysPerRow*3 +1, holesIndexes)
-    
-    holesIndexes = indexes
-    print holesIndexes
-    
-    skipPolygon = False
-    lines = f.readlines()
-    polyCount = -1
-    linesToSkip = 0
-    for line in lines:
-        if linesToSkip > 0:
-            linesToSkip-=1
-            continue
-        
-        if "Polygon" in line:
-            polyCount +=1
-            if polyCount in holesIndexes:
-                linesToSkip = 3
-                continue
-        f2.write(line)
     
 
 def texturify(modelFile, outputFile, textures, tiles, startAtOffset=1):
@@ -117,47 +90,96 @@ def texturify(modelFile, outputFile, textures, tiles, startAtOffset=1):
             tile = int(tile.rstrip())
             f2.write("    <TRef> { %s }\n" % tiles[group][tile])
             
-   
-def groupify(modelFile, outputFile=None, polysPerGroup=5, duplicationFactor=3,
-             rowPrefix="row", polyPrefix="tile-"):
-    """ Organise polysPerGroup polygons in a group """
+
+def groupify(inputFile, outputFile, polysPerGroup=5, \
+             prefix="row-", polyPrefix="tile-"):
+    data = EggData()
+    if data.read(Filename(inputFile)):
+        child = data.getFirstChild() # comment
+        curve = data.getNextChild()  # curve
+        vp = curve.getFirstChild() # vertex pool
+        groupCount = 0
+        isEof = False
+        while not isEof:
+            g = EggGroup("%s%d" % (prefix, groupCount))
+            for i in range(polysPerGroup):
+                poly = curve.getNextChild()
+                if poly is None:
+                    isEof = True
+                    break
+                poly.setName("%s%d" % (polyPrefix, i))
+                g.addChild(poly)
+            data.addChild(g)
+            
+            groupCount +=1
+        data.writeEgg(Filename(outputFile))
+    else:
+        print "wtf, data was wrong!"
     
-    if outputFile == None:
-        outputFile = "%s-grouped.%s" % (modelFile[:modelFile.index(".")],
-                                        modelFile[modelFile.index(".")+1:])
-    f = open(modelFile)
-    f2 = open(outputFile, "w")
 
-    inGroup = False
-    firstPolygon = True
-    polysCount = 0
-    polygonIndex = 0
-    lines = f.readlines()
-    for i,line in enumerate(lines):
-        if "Polygon" in line:
-            if firstPolygon:
-                f2.write("}\n") # close previous group
-                f2.write("<Group> %s-%d {\n" % (rowPrefix, 
-                                polysCount/(polysPerGroup*duplicationFactor)))
-                f2.write("  <Model> { 1 }\n")
-                #f2.write("  <Collide> { Polyset keep descend }\n")
-                firstPolygon = False
-            elif polysCount % polysPerGroup == 0:
-                f2.write("}\n")
-                f2.write("<Group> %s-%d {\n" % (rowPrefix, 
-                                polysCount/(polysPerGroup*duplicationFactor)))
-                #f2.write("  <Collide> { Polyset keep descend }\n")
-                f2.write("  <Model> { 1 }\n")
-            
-            polysCount+=1
-            
-            line = "  <Polygon> %s%s {\n" % (polyPrefix, polygonIndex)
-            if polygonIndex < polysPerGroup -1:
-                polygonIndex += 1
-            else:
-                polygonIndex = 0
-            
-        f2.write(line)
+def holeify(inputFile, outputFile, holesIndexes, prefix="row-", 
+            tilePrefix="tile-", polysPerRow=5):
+    
+    data = EggData()
+    if data.read(Filename(inputFile)):
+        child = data.getFirstChild()
+        child = data.getNextChild()
+        polys = 0
+        while child is not None:
+            child = data.getNextChild()
+            if type(child) is EggGroup:
+                for holeIndex in holesIndexes:
+                    if holeIndex in range(polys, polys+polysPerRow):
+                        idxInGroup = holeIndex - polys
+                        needle = "%s%d" % (tilePrefix, idxInGroup)
+                        poly = child.getFirstChild()
+                        if needle in poly.getName():
+                            child.removeChild(poly)
+                        poly = child.getNextChild()
+                        toRemove = []
+                        while poly is not None:
+                            if needle in poly.getName():
+                                toRemove.append(poly)
+                            poly = child.getNextChild()
+                        for p in toRemove:
+                            child.removeChild(p)
+                        #poly = child.findChild()
+                    
+                polys += polysPerRow
+    data.writeEgg(Filename(outputFile))
 
-    f.close()
-    f2.close()
+
+def enlarge(inputFile, outputFile, groupsForRow = 3, polysPerRow=5, \
+            rowPrefix="row-"):
+    
+    data = EggData()
+    if data.read(Filename(inputFile)):
+        child = data.getFirstChild() # comment
+        child = data.getNextChild()  # curve
+        
+        numOccurr = {}
+        currentNum = 0
+        while child is not None:
+            child = data.getNextChild()
+            if type(child) is EggGroup:
+                groupName = child.getName()
+                groupNum = groupName[groupName.index("-")+1:]
+                size = child.size()
+                newName = "%s%d" % (rowPrefix, currentNum)
+                if size < 5:
+                    currentNum += 1
+                else:
+                    if numOccurr.has_key(currentNum):
+                        if numOccurr[currentNum] == 2:
+                            currentNum +=1
+                        else:
+                            numOccurr[currentNum]+= 1
+                            
+                    else:
+                        numOccurr[currentNum] = 1
+                            
+                child.setName(newName)
+                    
+                        
+                    
+    data.writeEgg(Filename(outputFile))
