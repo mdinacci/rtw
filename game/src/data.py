55fdd76532af712c660834c29002406c94298dd1
Marco Dinacci <dev@dinointeractive.com>
2009-04-26 20:52:12 +0200

    minor stuff

--- game/src/data.py
+++ game/src/data.py
@@ -21,13 +21,18 @@ class TrackResult:
         (self.tid, self.bestTime, self.attempts, self.bid)
 
 class TrackInfo:
+    ATTRIBUTES = ("gold", "silver", "bronze", "limit")
+    
     tid = -1
     order = -1
     name = ""
     gold = -1
     silver = -1
     bronze = -1
+    limit = -1
 
     def __repr__(self):
-        return "ID: %s\nOrder: %s\nName: %s\nGold: %s\nSilver: %s\nBronze: %s\n" % \
-        (self.tid, self.order, self.name, self.gold, self.silver, self.bronze)
+        return "ID: %s\nOrder: %s\nName: %s\nGold: %s\nSilver: %s\nBronze: \
+        %s\nLimit: %s\n" % \
+        (self.tid, self.order, self.name, self.gold, self.silver, self.bronze, 
+         self.limit)
